feb5b69cc9cd22632cc65ef22b6295ec4d3be607
Bikal Gurung <gbikal@gmail.com>
2020-07-03 17:38:35 +0100

    wip multipart formdata parser

--- lib/lexer.mli
+++ lib/lexer.mli
@@ -1,1 +1,8 @@
-val lex_content_type : Lexing.lexbuf -> Parser.token
+type state =
+  | Multipart_formdata
+  | Multipart_body_part
+  | Multipart_body_header_param
+
+val lex_multipart_header : Lexing.lexbuf -> Parser.token
+
+val lex_multipart_formdata : state ref -> Lexing.lexbuf -> Parser.token
