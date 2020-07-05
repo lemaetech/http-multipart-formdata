4419ab5db2ab75afc7688d8305e9bd7a6916bf97
Bikal Gurung <gbikal@gmail.com>
2020-07-05 19:28:18 +0100

    add char_code.ml. add 'lex_multipart_header_value' skeleton code

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -13,54 +13,6 @@ type t =
   ; mutable rd_offset : int (* reading offset (position after current character) *)
   }
 
-module Char_code = struct
-  let space = 0x20
-  let htab = 0x09
-  let null = 0x00
-  let lparen = 0x28
-  let rparen = 0x29
-  let invalid = -1
-  let less_than = 0x3C
-  let greater_than = 0x3E
-  let at = 0x40
-  let comma = 0x2C
-  let colon = 0x3A
-  let semicolon = 0x3B
-  let forward_slash = (* / *) 0x2F
-  let back_slash = (* \ *) 0x5C
-  let double_quote = 0x22
-  let lbracket = 0x5B
-  let rbracket = 0x5D
-  let question = 0x3F
-  let equal = 0x3D
-  let is_vchar ch = ch >= 0x21 && ch <= 0x7E
-  let is_whitespace ch = ch == space || ch == htab
-
-  let is_tspecials ch =
-    ch == lparen
-    || ch == rparen
-    || ch == less_than
-    || ch == greater_than
-    || ch == at
-    || ch == comma
-    || ch == semicolon
-    || ch == colon
-    || ch == back_slash
-    || ch == double_quote
-    || ch == forward_slash
-    || ch == lbracket
-    || ch == rbracket
-    || ch == question
-    || ch == equal
-
-
-  let is_control ch = (ch >= 0x00 && ch <= 0x1F) || ch == 0x7F
-  let is_ascii ch = ch >= 0x00 && ch <= 0x7F
-
-  let is_token_char ch =
-    is_ascii ch && ch <> space && (not (is_control ch)) && not (is_tspecials ch)
-end
-
 let next t =
   if t.rd_offset < t.src_len
   then (
@@ -84,16 +36,13 @@ let create b =
   t
 
 
-let substring t start_offs =
-  String.sub t.src start_offs (t.offset - start_offs) |> String.trim
-
+let substring t start_offs = String.sub t.src start_offs (t.offset - start_offs)
 
 let rec lex_whitespace t =
   if Char_code.is_whitespace t.ch
   then (
     next t;
     lex_whitespace t)
-  else ()
 
 
 (*
@@ -133,11 +82,45 @@ let lex_token t =
   substring t start_offs |> Token.token
 
 
-let lex_header_param _t = ()
-let lex_formdata _t = ()
+(* 
+ https://tools.ietf.org/html/rfc5322#section-3
+
+ qtext           = %d33 /             ; Printable US-ASCII
+                   %d35-91 /          ;  characters not including
+                   %d93-126 /         ;  '\' or the quote character
+                   obs-qtext
+
+ qcontent        = qtext / quoted-pair
+
+ quoted-string   = [CFWS]
+                   DQUOTE *([FWS] qcontent) [FWS] DQUOTE
+                   [CFWS]
+*)
+
+(*  
+ FWS             =   ([*WSP CRLF] 1*WSP) /  obs-FWS
+                                           ; Folding white space
+*)
+(* let lex_fws t = *)
+(*   let rec lex () = *)
+(*    lex_whitespace t; *)
+(*      if peek t == *)
+
+let lex_quoted_string _t =
+  (* quoted-pair = ('\' (VCHAR / WSP)) / obs-qp *)
+  ()
+
+
+let lex_header_param _t = Token.eof
 
 (* tokenizes HTTP 'Content-Type' header value, eg. multipart/form-data; boundary=7353230 *)
-let lex_multipart_header_value _t = ()
+let lex_multipart_header_value t =
+  if Char_code.is_vchar t.ch
+  then lex_field_value t
+  else if t.ch == Char_code.semicolon
+  then lex_header_param t
+  else Token.eof
+
 
 (*--------------------------------------------*)
 (*----------------- Unit Tests ---------------*)
