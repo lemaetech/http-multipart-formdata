75a897afa19ae2fa6bdcf51955cd2e5804ab9cba
Bikal Gurung <gbikal@gmail.com>
2020-07-06 00:37:03 +0100

    add lex_buffer.ml

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -1,45 +1,16 @@
 open Std
 
 type mode =
-  | Multipart_formdata
-  | Multipart_body_part
-  | Multipart_body_header_param
+  | Content_type
+  | Content_type_param
 
-type t =
-  { src : string
-  ; src_len : int
-  ; mutable ch : int (* current character *)
-  ; mutable offset : int (* character offset *)
-  ; mutable rd_offset : int (* reading offset (position after current character) *)
-  }
+type lb = mode Lex_buffer.t
 
-let next t =
-  if t.rd_offset < t.src_len
+let rec lex_whitespace (lexer : lb) =
+  if Char_code.is_whitespace lexer.ch
   then (
-    t.offset <- t.rd_offset;
-    t.ch <- int_of_char t.src.[t.rd_offset];
-    t.rd_offset <- t.rd_offset + 1)
-  else (
-    t.offset <- t.src_len;
-    t.ch <- Char_code.invalid)
-
-let peek t =
-  if t.rd_offset < t.src_len then int_of_char t.src.[t.rd_offset] else Char_code.invalid
-
-let create b =
-  let t =
-    { src = b; src_len = String.length b; ch = Char_code.null; offset = 0; rd_offset = 0 }
-  in
-  next t;
-  t
-
-let substring t start_offs = String.sub t.src start_offs (t.offset - start_offs)
-
-let rec lex_whitespace t =
-  if Char_code.is_whitespace t.ch
-  then (
-    next t;
-    lex_whitespace t)
+    Lex_buffer.next lexer;
+    lex_whitespace lexer)
 
 (*
  field-content = field-vchar [ 1*( SP / HTAB ) field-vchar ]
@@ -47,34 +18,35 @@ let rec lex_whitespace t =
  field-vchar = VCHAR / obs-text
 *)
 let lex_field_value t =
-  let start_offs = t.offset in
+  Lex_buffer.lex_start t;
   let rec lex () =
     if Char_code.is_vchar t.ch
     then (
-      next t;
+      Lex_buffer.next t;
       lex ())
     else if Char_code.is_whitespace t.ch
     then (
+      Lex_buffer.next t;
       lex_whitespace t;
-      if Char_code.is_vchar @@ peek t
+      if Char_code.is_vchar @@ Lex_buffer.peek t
       then (
-        next t;
+        Lex_buffer.next t;
         lex ()))
   in
   lex ();
-  substring t start_offs |> Token.header_field_value
+  Lex_buffer.lexeme t |> Token.header_field_value
 
 (* token := 1*<any (US-ASCII) CHAR except SPACE, CTLs, or tspecials> *)
 let lex_token t =
-  let start_offs = t.offset in
+  Lex_buffer.lex_start t;
   let rec lex () =
     if Char_code.is_token_char t.ch
     then (
-      next t;
+      Lex_buffer.next t;
       lex ())
   in
   lex ();
-  substring t start_offs |> Token.token
+  Lex_buffer.lexeme t |> Token.token
 
 (* 
  https://tools.ietf.org/html/rfc5322#section-3
@@ -107,12 +79,12 @@ let lex_quoted_string _t =
 let lex_header_param _t = Token.eof
 
 (* tokenizes HTTP 'Content-Type' header value, eg. multipart/form-data; boundary=7353230 *)
-let lex_multipart_header_value t =
+let lex_multipart_header_value (t : lb) =
   if Char_code.is_vchar t.ch
   then lex_field_value t
   else if t.ch == Char_code.semicolon
   then (
-    next t;
+    Lex_buffer.next t;
     lex_header_param t)
   else Token.eof
 
@@ -124,7 +96,7 @@ let pp = Token.pp Format.std_formatter
 
 let%expect_test "lex_field_value" =
   [ "gzip, deflate ;"; "feed  "; "text/html ; " ]
-  |> List.map create
+  |> List.map (Lex_buffer.create Content_type)
   |> List.iter (lex_field_value >> pp);
   [%expect
     {|
@@ -133,6 +105,6 @@ let%expect_test "lex_field_value" =
 
 let%expect_test "lex_token" =
   [ "boundary ="; "bound\x7Fary"; "boundary"; "boundary    " ]
-  |> List.map create
+  |> List.map (Lex_buffer.create Content_type)
   |> List.iter (lex_token >> pp);
   [%expect {| (Token boundary)(Token bound)(Token boundary)(Token boundary) |}]
