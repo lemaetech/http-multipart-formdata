2df9556ad8eb7f049239573614db813f76c42db2
Bikal Gurung <gbikal@gmail.com>
2020-07-08 18:34:21 +0100

    fmt

--- lib/content_type.mli
+++ lib/content_type.mli
@@ -1,14 +1,14 @@
 type t
 
-val parse_content_type : Parser.t -> (t, string) Result.t
-
-val validate_boundary_value : string -> (unit, string) Result.t
-
 val ty : t -> string
 
 val subtype : t -> string
 
 val parameters : t -> (string * string) Seq.t
+
+val parse_content_type : Parser.t -> (t, string) Result.t
+
+val validate_boundary_value : string -> (unit, string) Result.t
 
 val find_parameter : name:string -> t -> string option
 
