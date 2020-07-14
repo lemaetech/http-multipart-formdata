4c0d4ec3d4e1825666ee1db147b91954ee1a8da3
Bikal Gurung <gbikal@gmail.com>
2020-07-14 17:47:52 +0100

    remove redundant files

--- lib/std.ml
+++ lib/std.ml
@@ -1,7 +1,7 @@
 (** [f1 >> f2] is same as [fun x -> f x |> g]. *)
 let[@inline] ( >> ) f g x = g (f x)
 
-let sprintf = Printf.sprintf
+let sprintf = Format.sprintf
 
 let asprintf = Format.asprintf
 
@@ -26,18 +26,4 @@ end
 
 module String = struct
   include StringLabels
-
-  let starts_with ~prefix s =
-    let prefix_len = length prefix in
-    let full_len = length s in
-    full_len >= prefix_len && String.equal (sub s ~pos:0 ~len:prefix_len) prefix
-
-  let chop_prefix ~prefix s =
-    let prefix_len = length prefix in
-    let full_len = length s in
-    if
-      full_len >= prefix_len
-      && String.equal (sub s ~pos:0 ~len:prefix_len) prefix
-    then sub s ~pos:prefix_len ~len:(full_len - prefix_len)
-    else s
 end
