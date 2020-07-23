0b9acfdf92b263d01b2943d1658e4910b0724f88
Bikal Gurung <gbikal@gmail.com>
2020-07-23 18:45:28 +0100

    make error private type

--- lib/multipart_formdata.mli
+++ lib/multipart_formdata.mli
@@ -3,7 +3,8 @@ type file
 type t = [ `File of file list | `String of string list ]
 
 type error =
-  [ `Boundary_parameter_not_found
+  private
+  [> `Boundary_parameter_not_found
   | `Not_multipart_formdata_header
   | `Invalid_multipart_body_header
   | Reparse.Parser.error ]
@@ -13,4 +14,4 @@ type header
 val parse :
   header:string ->
   body:Reparse.Parser.input ->
-  ((header list * string) list, [> error ]) result
+  ((header list * string) list, error) result
