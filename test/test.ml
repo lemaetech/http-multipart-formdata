30d2ef3d063fddd1bd19c30706dd26ca75259e4d
Bikal Lem <gbikal@gmail.com>
2020-07-30 19:18:15 +0100

    isolate test to 1 part

--- test/test.ml
+++ test/test.ml
@@ -16,26 +16,26 @@ let%expect_test _ =
   let body =
     [
       {||};
-      {|-----------------------------735323031399963166993862150|};
-      {|Content-Disposition: form-data; name="text1"|};
-      {||};
-      {|text default|};
-      {|-----------------------------735323031399963166993862150|};
-      {|Content-Disposition: form-data; name="text2"|};
-      {||};
-      {|aωb|};
-      {|-----------------------------735323031399963166993862150|};
-      {|Content-Disposition: form-data; name="file1"; filename="a.txt"|};
-      {|Content-Type: text/plain|};
-      {||};
-      {|Content of a.txt.|};
-      {||};
-      {|-----------------------------735323031399963166993862150|};
-      {|Content-Disposition: form-data; name="file2"; filename="a.html"|};
-      {|Content-Type: text/html|};
-      {||};
-      {|<!DOCTYPE html><title>Content of a.html.</title>|};
-      {||};
+      (* {|-----------------------------735323031399963166993862150|}; *)
+      (* {|Content-Disposition: form-data; name="text1"|}; *)
+      (* {||}; *)
+      (* {|text default|}; *)
+      (* {|-----------------------------735323031399963166993862150|}; *)
+      (* {|Content-Disposition: form-data; name="text2"|}; *)
+      (* {||}; *)
+      (* {|aωb|}; *)
+      (* {|-----------------------------735323031399963166993862150|}; *)
+      (* {|Content-Disposition: form-data; name="file1"; filename="a.txt"|}; *)
+      (* {|Content-Type: text/plain|}; *)
+      (* {||}; *)
+      (* {|Content of a.txt.|}; *)
+      (* {||}; *)
+      (* {|-----------------------------735323031399963166993862150|}; *)
+      (* {|Content-Disposition: form-data; name="file2"; filename="a.html"|}; *)
+      (* {|Content-Type: text/html|}; *)
+      (* {||}; *)
+      (* {|<!DOCTYPE html><title>Content of a.html.</title>|}; *)
+      (* {||}; *)
       {|-----------------------------735323031399963166993862150|};
       {|Content-Disposition: form-data; name="file3"; filename="binary"|};
       {|Content-Type: application/octet-stream|};
@@ -48,7 +48,8 @@ let%expect_test _ =
   Multipart_formdata.parse ~header ~body:(`String body)
   |> pp Format.std_formatter;
 
-  [%expect {|
+  [%expect
+    {|
     (Ok
       (((form_field file3) (filename (binary)) (content_type text/plain)
          (parameters ((name file3) (filename binary)))
@@ -57,18 +58,4 @@ let%expect_test _ =
            \nContent-Type: application/octet-stream\r\
            \n\r\
            \na\207\137b\r\
-           \n"))
-        ((form_field file1) (filename (a.txt)) (content_type text/plain)
-          (parameters ((name file1) (filename a.txt)))
-          (body
-             "\r\
-            \nContent-Type: text/plain\r\
-            \n\r\
-            \nContent of a.txt.\r\
-            \n\r\
-            \n"))
-        ((form_field text1) (filename ()) (content_type text/plain)
-          (parameters ((name text1))) (body  "\r\
-                                            \n\r\
-                                            \ntext default\r\
-                                            \n"))))|}]
+           \n"))))|}]
