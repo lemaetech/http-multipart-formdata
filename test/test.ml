b92093c508de990f3e535b7e40ee02e8514d3d09
Bikal Lem <gbikal@gmail.com>
2020-07-31 12:47:48 +0100

    add parts with same form name

--- test/test.ml
+++ test/test.ml
@@ -83,3 +83,78 @@ let%expect_test _ =
                 \n\r\
                 \na\207\137b\r\
                 \n"))))|}]
+
+let%expect_test "multiple body parts with same form field." =
+  let header =
+    " multipart/form-data; \
+     boundary=---------------------------735323031399963166993862150"
+  in
+  let body =
+    [
+      {||};
+      {|-----------------------------735323031399963166993862150|};
+      {|Content-Disposition: form-data; name="text1"|};
+      {||};
+      {|text default|};
+      {|-----------------------------735323031399963166993862150|};
+      {|Content-Disposition: form-data; name="text1"|};
+      {||};
+      {|aωb|};
+      {|-----------------------------735323031399963166993862150|};
+      {|Content-Disposition: form-data; name="file1"; filename="a.txt"|};
+      {|Content-Type: text/plain|};
+      {||};
+      {|Content of a.txt.|};
+      {||};
+      {|-----------------------------735323031399963166993862150|};
+      {|Content-Disposition: form-data; name="file1"; filename="a.html"|};
+      {|Content-Type: text/html|};
+      {||};
+      {|<!DOCTYPE html><title>Content of a.html.</title>|};
+      {||};
+      {|-----------------------------735323031399963166993862150|};
+      {|Content-Disposition: form-data; name="file1"; filename="binary"|};
+      {|Content-Type: application/octet-stream|};
+      {||};
+      {|aωb|};
+      {|-----------------------------735323031399963166993862150--|};
+    ]
+    |> String.concat "\r\n"
+  in
+  Http_multipart_formdata.parse ~header ~body:(`String body)
+  |> pp Format.std_formatter;
+
+  [%expect {|
+    (Ok
+      (((form_field text1) (filename ()) (content_type text/plain)
+         (parameters ((name text1))) (body  "\r\
+                                           \n\r\
+                                           \ntext default\r\
+                                           \n"))
+        ((form_field text1) (filename ()) (content_type text/plain)
+          (parameters ((name text1))) (body  "\r\
+                                            \n\r\
+                                            \na\207\137b\r\
+                                            \n"))
+        ((form_field file1) (filename (a.txt)) (content_type text/plain)
+          (parameters ((filename a.txt) (name file1)))
+          (body  "\r\
+                \n\r\
+                \nContent of a.txt.\r\
+                \n\r\
+                \n"))
+        ((form_field file1) (filename (a.html)) (content_type text/html)
+          (parameters ((filename a.html) (name file1)))
+          (body
+             "\r\
+            \n\r\
+            \n<!DOCTYPE html><title>Content of a.html.</title>\r\
+            \n\r\
+            \n"))
+        ((form_field file1) (filename (binary))
+          (content_type application/octet-stream)
+          (parameters ((filename binary) (name file1)))
+          (body  "\r\
+                \n\r\
+                \na\207\137b\r\
+                \n")))) |}]
