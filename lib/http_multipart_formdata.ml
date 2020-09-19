ff84c33480ea846dbbd4d3b19a9e29934dc475d6
Bikal Lem <315907+bikallem@users.noreply.github.com>
2020-09-19 10:43:59 +0100

    Upgrade to new reparse. (#7)
    * upgrade reparse
    
    * fix c_comment
    
    * remove p_ use R. as prefix for Reparse functions
    
    * remove open Reparse
    
    * fix loop_body
    
    * make all tests pass

--- lib/http_multipart_formdata.ml
+++ lib/http_multipart_formdata.ml
@@ -6,9 +6,10 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
  *-------------------------------------------------------------------------*)
-open Reparse
 open Sexplib0
 open Sexplib0.Sexp_conv
+open Reparse.Infix
+module R = Reparse
 
 exception Http_multipart_formdata of string
 
@@ -27,10 +28,10 @@ end
 
 module File_part = struct
   type t =
-    { filename: string option
-    ; content_type: string
-    ; parameters: string String_map.t
-    ; body: bytes }
+    { filename : string option
+    ; content_type : string
+    ; parameters : string String_map.t
+    ; body : bytes }
   [@@deriving sexp_of]
 
   let filename t = t.filename
@@ -42,179 +43,278 @@ end
 
 type t = part list String_map.t [@@deriving sexp_of]
 
-and part = File of File_part.t | String of string [@@deriving sexp_of]
+and part =
+  | File   of File_part.t
+  | String of string
+[@@deriving sexp_of]
 
 let pp fmt t = Sexp.pp_hum_indent 2 fmt (sexp_of_t t)
 
 type part_header =
-  | Content_type of
-      {ty: string; subtype: string; parameters: string String_map.t}
+  | Content_type        of
+      { ty : string
+      ; subtype : string
+      ; parameters : string String_map.t }
   | Content_disposition of string String_map.t
 
 let is_alpha_digit = function
-  | '0' .. '9' | 'a' .. 'z' | 'A' .. 'Z' -> true
-  | _ -> false
-
-let is_space c = c == '\x20'
-let is_control = function '\x00' .. '\x1F' | '\x7F' -> true | _ -> false
-
-let is_tspecials = function
-  | '(' | ')' | '<' | '>' | '@' | ',' | ';' | ':' | '\\' | '"' | '/' | '['
-   |']' | '?' | '=' ->
+  | '0' .. '9'
+  | 'a' .. 'z'
+  | 'A' .. 'Z' ->
       true
   | _ -> false
 
-let is_ascii_chars = function '\x00' .. '\x7F' -> true | _ -> false
-let is_vchar = function '\x21' .. '\x7E' -> true | _ -> false
-let is_whitespace = function '\x20' | '\x09' -> true | _ -> false
+let is_space c = c == '\x20'
+
+let is_control = function
+  | '\x00' .. '\x1F'
+  | '\x7F' ->
+      true
+  | _ -> false
+
+let is_tspecial = function
+  | '('
+  | ')'
+  | '<'
+  | '>'
+  | '@'
+  | ','
+  | ';'
+  | ':'
+  | '\\'
+  | '"'
+  | '/'
+  | '['
+  | ']'
+  | '?'
+  | '=' ->
+      true
+  | _ -> false
+
+let is_ascii_char = function
+  | '\x00' .. '\x7F' -> true
+  | _                -> false
 
 let is_ctext = function
-  | '\x21' .. '\x27' | '\x2A' .. '\x5B' | '\x5D' .. '\x7E' -> true
+  | '\x21' .. '\x27'
+  | '\x2A' .. '\x5B'
+  | '\x5D' .. '\x7E' ->
+      true
   | _ -> false
 
 let is_qtext = function
-  | '\x21' | '\x23' .. '\x5B' | '\x5D' .. '\x7E' -> true
+  | '\x21'
+  | '\x23' .. '\x5B'
+  | '\x5D' .. '\x7E' ->
+      true
   | _ -> false
 
 let is_token_char c =
-  is_ascii_chars c
+  is_ascii_char c
   && (not (is_space c))
   && (not (is_control c))
-  && not (is_tspecials c)
+  && not (is_tspecial c)
 
-let p_whitespace = skip_while is_whitespace
+let implode l = List.to_seq l |> String.of_seq
 
-let p_token =
-  satisfy is_token_char
-  >>= fun ch ->
-  take_while is_token_char >>= fun chars -> String.make 1 ch ^ chars |> ok
+let token =
+  R.take ~at_least:1 (R.satisfy is_token_char)
+  >|= fun (_, chars) -> implode chars
 
 (* https://tools.ietf.org/html/rfc5322#section-3.2.1
-   quoted-pair     =   ('\' (VCHAR / WSP)) / obs-qp
-*)
-let p_quoted_pair =
-  char '\\' *> satisfy (fun c -> is_whitespace c || is_vchar c)
-  >>| fun c -> String.make 1 '\\' ^ String.make 1 c
+   quoted-pair     =   ('\' (VCHAR / WSP)) / obs-qp *)
+let quoted_pair = String.make 1 <$> R.char '\\' *> (R.whitespace <|> R.vchar)
 
-(* https://tools.ietf.org/html/rfc5322#section-3.2.2 *)
-let p_fws =
-  count_skip_while is_whitespace
+(* 
+  Folding whitespace and comments - https://tools.ietf.org/html/rfc5322#section-3.2.2 
+
+  let r = R.parse "    \r\n    " fws
+  r = " "
+ *)
+let fws =
+  R.skip R.whitespace
   >>= fun ws_count1 ->
-  count_skip_while_string 3 (fun s ->
-      String.equal s "\x0D\x0A\x20" || String.equal s "\x0D\x0A\x09")
-  >>= fun lws_count ->
-  count_skip_while is_whitespace
-  >>| fun ws_count2 -> if ws_count1 + lws_count + ws_count2 > 0 then " " else ""
+  R.skip (R.string "\r\n" *> R.skip ~at_least:1 R.whitespace)
+  >|= fun lws_count -> if ws_count1 + lws_count > 0 then " " else ""
 
-let p_comment =
-  let ctext = satisfy is_ctext >>| String.make 1 in
-  let rec loop_comment () =
-    char '('
-    *> many
-         ( p_fws
-         >>= fun sp ->
-         ctext
-         <|> p_quoted_pair
-         <|> (loop_comment () >>| fun txt -> "(" ^ txt ^ ")")
-         >>| ( ^ ) sp )
-    >>| String.concat ""
-    >>= fun comment_text ->
-    p_fws >>= fun sp -> ok @@ comment_text ^ sp <* char ')' in
-  loop_comment ()
+(*
+  let r = R.parse "(    asdfasdfasdfasd(aaa) \\(cccc\\) (bbb(ddd)))" comments;;
+  r = " asdfasdfasdfasd;aaa (cccc) ;bbb;ddd";;
+ *)
+let comment =
+  let ctext = R.satisfy is_ctext >|= String.make 1 in
+  let rec loop_comments () =
+    let ccontent =
+      R.take
+        (R.map2
+           (fun sp content -> sp ^ content)
+           fws
+           (R.any
+              [ lazy ctext
+              ; lazy quoted_pair
+              ; lazy (loop_comments () >|= ( ^ ) ";") ]))
+      >|= fun (_, s) -> String.concat "" s
+    in
+    R.char '(' *> R.map2 (fun comment_txt sp -> comment_txt ^ sp) ccontent fws
+    <* R.char ')'
+  in
+  loop_comments ()
 
-let p_cfws =
-  many (p_fws >>= fun sp -> p_comment >>| fun comment_text -> sp ^ comment_text)
-  >>= (fun l ->
-        p_fws >>| fun sp -> if String.length sp > 0 then l @ [sp] else l)
-  <|> (p_fws >>| fun sp -> if String.length sp > 0 then [sp] else [])
+(*
+  let r = R.parse "  (    asdfasdfasdfasd(aaa) \\(cccc\\) (bbb(ddd)))   " cfws;;
+  r = " "
+ *)
+let cfws =
+  let one_or_more_comments =
+    R.take
+      ~at_least:1
+      (R.map2 (fun sp comment_txt -> sp ^ comment_txt) fws comment)
+    *> fws
+  in
+  one_or_more_comments <|> fws
 
-let p_quoted_string =
-  let qcontent = satisfy is_qtext >>| String.make 1 <|> p_quoted_pair in
-  p_cfws *> char '"' *> many (p_fws >>= fun sp -> qcontent >>| ( ^ ) sp)
-  >>| String.concat ""
-  >>= fun q_string -> p_fws >>| (fun sp -> q_string ^ sp) <* char '"'
+(* Test cases : 
+  let r = R.parse "\"    \r\n            hello   \r\n          \"" quoted_string;;
+  r = " hello ";;
 
-let p_param_value = p_token <|> p_quoted_string
+  R.parse "(comment1)\"hello\"(comment2)" quoted_string;;
+  r = "hello";;
+*)
+let quoted_string =
+  let qtext = String.make 1 <$> R.satisfy is_qtext in
+  let qcontent =
+    (fun (_, l) -> String.concat "" l)
+    <$> R.take
+          (R.map2
+             (fun sp qcontent' -> sp ^ qcontent')
+             fws
+             (qtext <|> quoted_pair))
+  in
+  cfws *> R.dquote *> R.map2 (fun qcontent' sp -> qcontent' ^ sp) qcontent fws
+  <* R.dquote
+  <* cfws
 
-let p_param =
-  p_whitespace *> char ';' *> p_whitespace *> p_token
-  >>= fun attribute ->
-  char '=' *> p_param_value >>| fun value -> (attribute, value)
+(*
+  let r = R.parse "asdfasdf" p_param_value;;
+  r = "asdfasdf";;
+
+  R.parse "\"hello\"" p_param_value;;
+  r = "hello"
+ *)
+let param_value = token <|> quoted_string
+
+(*
+  let r = R.parse "; field1=value1;" p_param;;       
+  r = ("field1", "value1");;
+
+  let r = R.parse "; field1=\"value1\";" p_param;;
+  r = ("field1", "value1");;
+ *)
+let param =
+  let name =
+    R.skip R.whitespace *> R.char ';' *> R.skip R.whitespace *> token
+  in
+  let value = R.char '=' *> param_value in
+  R.map2 (fun name value -> (name, value)) name value
 
 let p_restricted_name =
-  let is_restricted_name_chars = function
-    | '!' | '#' | '$' | '&' | '-' | '^' | '_' | '.' | '+' -> true
-    | c when is_alpha_digit c -> true
-    | _ -> false in
-  satisfy is_alpha_digit
+  let p_restricted_name_chars =
+    R.satisfy (function
+        | '!'
+        | '#'
+        | '$'
+        | '&'
+        | '-'
+        | '^'
+        | '_'
+        | '.'
+        | '+' ->
+            true
+        | c when is_alpha_digit c -> true
+        | _ -> false)
+  in
+  R.satisfy is_alpha_digit
   >>= fun first_ch ->
   let buf = Buffer.create 10 in
   Buffer.add_char buf first_ch ;
-  take_while_n 126 is_restricted_name_chars
-  >>= fun restricted_name ->
-  Buffer.add_string buf restricted_name ;
-  ok @@ Buffer.contents buf
+  R.take ~up_to:126 p_restricted_name_chars
+  >|= fun (_, restricted_name) ->
+  Buffer.add_string buf (implode restricted_name) ;
+  Buffer.contents buf
 
-let p_content_disposition =
-  string "Content-Disposition"
-  *> char ':'
-  *> p_whitespace
-  *> string "form-data"
-  *> many p_param
-  >>| fun params ->
+let content_disposition =
+  R.string "Content-Disposition:"
+  *> R.skip R.whitespace
+  *> R.string "form-data"
+  *> R.take param
+  >|= fun (_, params) ->
   let params = List.to_seq params |> String_map.of_seq in
   Content_disposition params
 
-let p_content_type parse_header_name =
-  ( if parse_header_name then string "Content-Type" *> char ':' *> ok ()
-  else ok () )
-  *> p_whitespace
+let content_type parse_header_name =
+  (if parse_header_name then R.string "Content-Type:" *> R.unit else R.unit)
+  *> R.skip R.whitespace
   *> p_restricted_name
   >>= fun ty ->
-  char '/' *> p_restricted_name
+  R.char '/' *> p_restricted_name
   >>= fun subtype ->
-  p_whitespace *> many p_param
-  >>| fun params ->
+  R.take param
+  >|= fun (_, params) ->
   let parameters = params |> List.to_seq |> String_map.of_seq in
   Content_type {ty; subtype; parameters}
 
-let p_header_boundary =
+let header_boundary =
   let is_bcharnospace = function
-    | '\'' | '(' | ')' | '+' | '_' | ',' | '-' | '.' | '/' | ':' | '=' | '?' ->
+    | '\''
+    | '('
+    | ')'
+    | '+'
+    | '_'
+    | ','
+    | '-'
+    | '.'
+    | '/'
+    | ':'
+    | '='
+    | '?' ->
         true
     | c when is_alpha_digit c -> true
-    | _ -> false in
-  let is_bchars = function
-    | '\x20' -> true
-    | c when is_bcharnospace c -> true
-    | _ -> false in
-  let is_dquote = function '"' -> true | _ -> false in
+    | _ -> false
+  in
+  let bchars =
+    R.satisfy (function
+        | '\x20' -> true
+        | c when is_bcharnospace c -> true
+        | _ -> false)
+  in
   let boundary =
-    take_while_n 70 is_bchars
-    >>= fun bchars ->
-    let len = String.length bchars in
+    R.take ~up_to:70 bchars
+    >>= fun (_, bchars) ->
+    let len = List.length bchars in
     if len > 0 then
-      let last_char = String.unsafe_get bchars (len - 1) in
-      if is_bcharnospace last_char then ok bchars
-      else fail `Invalid_last_char_boundary_value
-    else fail `Zero_length_boundary_value in
-  char_if is_dquote *> boundary <* char_if is_dquote <|> p_token
+      let last_char = List.nth bchars (len - 1) in
+      if is_bcharnospace last_char then R.return (implode bchars)
+      else R.fail "Invalid boundary value: invalid last char"
+    else R.fail "Invalid boundary value: 0 length"
+  in
+  R.optional R.dquote *> boundary <* R.optional R.dquote <|> token
 
-let p_multipart_formdata_header =
+let multipart_formdata_header =
   let param =
-    p_whitespace *> char ';' *> p_whitespace *> p_token
+    R.skip R.whitespace *> R.char ';' *> R.skip R.whitespace *> token
     >>= fun attribute ->
-    ( char '='
-    *> if attribute = "boundary" then p_header_boundary else p_param_value )
-    >>| fun value -> (attribute, value) in
-  ( string_if "\r\n"
-    *> string_if "Content-Type:"
-    *> p_whitespace
-    *> string "multipart/form-data"
-  >>*? `Not_multipart_formdata_header )
-  *> p_whitespace
-  *> many param
-  >>= fun params -> params |> List.to_seq |> String_map.of_seq |> ok
+    ( R.char '='
+    *> if attribute = "boundary" then header_boundary else param_value )
+    >|= fun value -> (attribute, value)
+  in
+  ( R.optional R.crlf
+    *> R.optional (R.string "Content-Type:")
+    *> R.skip R.whitespace
+    *> R.string "multipart/form-data"
+  <?> "Not multipart formdata header" )
+  *> R.skip R.whitespace
+  *> R.take param
+  >|= fun (_, params) -> params |> List.to_seq |> String_map.of_seq
 
 let body_part headers body =
   let name, content_type, filename, parameters =
@@ -235,9 +335,10 @@ let body_part headers body =
             , filename
             , String_map.union (fun _key a _b -> Some a) params params2 ))
       (None, None, None, String_map.empty)
-      headers in
+      headers
+  in
   match name with
-  | None -> Reparse.fail `Name_parameter_not_found
+  | None    -> R.fail "parameter 'name' not found"
   | Some nm ->
       let content_type = try Option.get content_type with _ -> "text/plain" in
       let parameters =
@@ -245,7 +346,8 @@ let body_part headers body =
         |> fun parameters ->
         match filename with
         | Some _ -> String_map.remove "filename" parameters
-        | None -> parameters in
+        | None   -> parameters
+      in
       ( match filename with
       | Some _ ->
           ( nm
@@ -253,51 +355,55 @@ let body_part headers body =
               { File_part.filename
               ; content_type
               ; parameters
-              ; body= Bytes.unsafe_of_string body } )
-      | None -> (nm, String body) )
-      |> Reparse.ok
+              ; body = Bytes.unsafe_of_string body } )
+      | None   -> (nm, String body) )
+      |> R.return
 
 let add_part (name, bp) m =
   match String_map.find_opt name m with
   | Some l -> String_map.add name (bp :: l) m
-  | None -> String_map.add name [bp] m
+  | None   -> String_map.add name [bp] m
 
-let p_multipart_bodyparts boundary_value =
+let multipart_bodyparts boundary_value =
   let dash_boundary = "--" ^ boundary_value in
+  let end_boundary = dash_boundary ^ "--" in
+  let line = R.line `CRLF in
   let rec loop_body buf =
     line
-    >>= function
-    | Some ln ->
-        if ln <> dash_boundary then (
-          Buffer.add_string buf (ln ^ "\r\n") ;
-          loop_body buf )
-        else (Buffer.contents buf, Some ln) |> ok
-    | None -> (Buffer.contents buf, None) |> ok in
-  let rec loop_parts parts = function
-    | Some ln ->
-        if ln = dash_boundary ^ "--" then ok parts
-        else if ln = dash_boundary then
-          many (string "\r\n" *> p_content_type true <|> p_content_disposition)
-          >>= fun headers ->
-          loop_body (Buffer.create 5)
-          >>= fun (body, ln) ->
-          body_part headers body >>= fun bp -> loop_parts (bp :: parts) ln
-        else line >>= loop_parts parts
-    | None -> ok parts in
-  line
-  >>= loop_parts []
-  >>= fun parts ->
+    >>= fun ln ->
+    if ln = dash_boundary then R.return (Buffer.contents buf, true)
+    else if ln = end_boundary then R.return (Buffer.contents buf, false)
+    else (
+      Buffer.add_string buf ln ;
+      Buffer.add_string buf "\r\n" ;
+      loop_body buf )
+  in
+  let rec loop_parts parts =
+    let headers =
+      R.take
+        ~at_least:1
+        (R.any [lazy content_disposition; lazy (content_type true)] <* R.crlf)
+      >|= snd
+      <* R.crlf
+    in
+    headers
+    >>= fun part_headers ->
+    loop_body (Buffer.create 0)
+    >>= fun (body, continue) ->
+    body_part part_headers body
+    >>= fun bp ->
+    if continue then loop_parts (bp :: parts) else R.return (bp :: parts)
+  in
+  R.crlf *> R.string dash_boundary *> R.crlf *> loop_parts []
+  >|= fun parts ->
   List.fold_left
     (fun m (name, bp) -> add_part (name, bp) m)
-    String_map.empty parts
-  |> ok
+    String_map.empty
+    parts
 
 let parse ~content_type_header ~body =
-  let header_params =
-    parse (`String content_type_header) p_multipart_formdata_header
-    |> Result.get_ok in
+  let header_params = R.parse content_type_header multipart_formdata_header in
   match String_map.find "boundary" header_params with
-  | boundary_value ->
-      parse body (p_multipart_bodyparts boundary_value) |> Result.get_ok
+  | boundary_value      -> R.parse body (multipart_bodyparts boundary_value)
   | exception Not_found ->
       raise @@ Http_multipart_formdata "Boundary paramater not found"
--- test/test.ml
+++ test/test.ml
@@ -1,145 +1,154 @@
 let%expect_test _ =
   let content_type_header =
     "Content-Type: multipart/form-data; \
-     boundary=---------------------------735323031399963166993862150" in
+     boundary=---------------------------735323031399963166993862150"
+  in
   let body =
-    [ {||}; {|-----------------------------735323031399963166993862150|}
-    ; {|Content-Disposition: form-data; name="text1"|}; {||}; {|text default|}
+    [ {||}
     ; {|-----------------------------735323031399963166993862150|}
-    ; {|Content-Disposition: form-data; name="text2"|}; {||}; {|aωb|}
+    ; {|Content-Disposition: form-data; name="text1"|}
+    ; {||}
+    ; {|text default|}
+    ; {|-----------------------------735323031399963166993862150|}
+    ; {|Content-Disposition: form-data; name="text2"|}
+    ; {||}
+    ; {|aωb|}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file1"; filename="a.txt"|}
-    ; {|Content-Type: text/plain|}; {||}; {|Content of a.txt.|}; {||}
+    ; {|Content-Type: text/plain|}
+    ; {||}
+    ; {|Content of a.txt.|}
+    ; {||}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file2"; filename="a.html"|}
-    ; {|Content-Type: text/html|}; {||}
-    ; {|<!DOCTYPE html><title>Content of a.html.</title>|}; {||}
+    ; {|Content-Type: text/html|}
+    ; {||}
+    ; {|<!DOCTYPE html><title>Content of a.html.</title>|}
+    ; {||}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file3"; filename="binary"|}
-    ; {|Content-Type: application/octet-stream|}; {||}; {|aωb|}
+    ; {|Content-Type: application/octet-stream|}
+    ; {||}
+    ; {|aωb|}
     ; {|-----------------------------735323031399963166993862150--|} ]
-    |> String.concat "\r\n" in
+    |> String.concat "\r\n"
+  in
   Http_multipart_formdata.(
-    parse ~content_type_header ~body:(`String body) |> pp Format.std_formatter) ;
+    parse ~content_type_header ~body |> pp Format.std_formatter) ;
   [%expect
     {|
     ((file1
        ((File
           ((filename (a.txt)) (content_type text/plain) (parameters ())
-            (body  "\r\
-                  \n\r\
-                  \nContent of a.txt.\r\
+            (body  "Content of a.txt.\r\
                   \n\r\
                   \n")))))
       (file2
         ((File
            ((filename (a.html)) (content_type text/html) (parameters ())
-             (body
-                "\r\
-               \n\r\
-               \n<!DOCTYPE html><title>Content of a.html.</title>\r\
-               \n\r\
-               \n")))))
+             (body  "<!DOCTYPE html><title>Content of a.html.</title>\r\
+                   \n\r\
+                   \n")))))
       (file3
         ((File
            ((filename (binary)) (content_type application/octet-stream)
-             (parameters ()) (body  "\r\
-                                   \n\r\
-                                   \na\207\137b\r\
-                                   \n")))))
-      (text1 ((String  "\r\
-                      \n\r\
-                      \ntext default\r\
-                      \n")))
-      (text2 ((String  "\r\
-                      \n\r\
-                      \na\207\137b\r\
-                      \n"))))|}]
+             (parameters ()) (body "a\207\137b\r\n")))))
+      (text1 ((String "text default\r\n"))) (text2 ((String "a\207\137b\r\n"))))|}]
 
 let%expect_test "multiple body parts with same form field." =
   let content_type_header =
     " multipart/form-data; \
-     boundary=---------------------------735323031399963166993862150" in
+     boundary=---------------------------735323031399963166993862150"
+  in
   let body =
-    [ {||}; {|-----------------------------735323031399963166993862150|}
-    ; {|Content-Disposition: form-data; name="text1"|}; {||}; {|text default|}
+    [ {||}
     ; {|-----------------------------735323031399963166993862150|}
-    ; {|Content-Disposition: form-data; name="text1"|}; {||}; {|aωb|}
+    ; {|Content-Disposition: form-data; name="text1"|}
+    ; {||}
+    ; {|text default|}
+    ; {|-----------------------------735323031399963166993862150|}
+    ; {|Content-Disposition: form-data; name="text1"|}
+    ; {||}
+    ; {|aωb|}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file1"; filename="a.txt"|}
-    ; {|Content-Type: text/plain|}; {||}; {|Content of a.txt.|}; {||}
+    ; {|Content-Type: text/plain|}
+    ; {||}
+    ; {|Content of a.txt.|}
+    ; {||}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file1"; filename="a.html"|}
-    ; {|Content-Type: text/html|}; {||}
-    ; {|<!DOCTYPE html><title>Content of a.html.</title>|}; {||}
+    ; {|Content-Type: text/html|}
+    ; {||}
+    ; {|<!DOCTYPE html><title>Content of a.html.</title>|}
+    ; {||}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file1"; filename="binary"|}
-    ; {|Content-Type: application/octet-stream|}; {||}; {|aωb|}
+    ; {|Content-Type: application/octet-stream|}
+    ; {||}
+    ; {|aωb|}
     ; {|-----------------------------735323031399963166993862150--|} ]
-    |> String.concat "\r\n" in
+    |> String.concat "\r\n"
+  in
   Http_multipart_formdata.(
-    parse ~content_type_header ~body:(`String body) |> pp Format.std_formatter) ;
+    parse ~content_type_header ~body |> pp Format.std_formatter) ;
   [%expect
     {|
     ((file1
        ((File
           ((filename (a.txt)) (content_type text/plain) (parameters ())
-            (body  "\r\
-                  \n\r\
-                  \nContent of a.txt.\r\
+            (body  "Content of a.txt.\r\
                   \n\r\
                   \n")))
          (File
            ((filename (a.html)) (content_type text/html) (parameters ())
-             (body
-                "\r\
-               \n\r\
-               \n<!DOCTYPE html><title>Content of a.html.</title>\r\
-               \n\r\
-               \n")))
+             (body  "<!DOCTYPE html><title>Content of a.html.</title>\r\
+                   \n\r\
+                   \n")))
          (File
            ((filename (binary)) (content_type application/octet-stream)
-             (parameters ()) (body  "\r\
-                                   \n\r\
-                                   \na\207\137b\r\
-                                   \n")))))
-      (text1
-        ((String  "\r\
-                 \n\r\
-                 \ntext default\r\
-                 \n")
-          (String  "\r\
-                  \n\r\
-                  \na\207\137b\r\
-                  \n")))) |}]
+             (parameters ()) (body "a\207\137b\r\n")))))
+      (text1 ((String "text default\r\n") (String "a\207\137b\r\n")))) |}]
 
 module SM = Http_multipart_formdata.String_map
 
 let%test "find/body_parts" =
   let content_type_header =
     " multipart/form-data; \
-     boundary=---------------------------735323031399963166993862150" in
+     boundary=---------------------------735323031399963166993862150"
+  in
   let body =
-    [ {||}; {|-----------------------------735323031399963166993862150|}
-    ; {|Content-Disposition: form-data; name="text1"|}; {||}; {|text default|}
+    [ {||}
     ; {|-----------------------------735323031399963166993862150|}
-    ; {|Content-Disposition: form-data; name="text1"|}; {||}; {|aωb|}
+    ; {|Content-Disposition: form-data; name="text1"|}
+    ; {||}
+    ; {|text default|}
+    ; {|-----------------------------735323031399963166993862150|}
+    ; {|Content-Disposition: form-data; name="text1"|}
+    ; {||}
+    ; {|aωb|}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file1"; filename="a.txt"|}
-    ; {|Content-Type: text/plain|}; {||}; {|Content of a.txt.|}; {||}
+    ; {|Content-Type: text/plain|}
+    ; {||}
+    ; {|Content of a.txt.|}
+    ; {||}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file1"; filename="a.html"|}
-    ; {|Content-Type: text/html|}; {||}
-    ; {|<!DOCTYPE html><title>Content of a.html.</title>|}; {||}
+    ; {|Content-Type: text/html|}
+    ; {||}
+    ; {|<!DOCTYPE html><title>Content of a.html.</title>|}
+    ; {||}
     ; {|-----------------------------735323031399963166993862150|}
     ; {|Content-Disposition: form-data; name="file1"; filename="binary"|}
-    ; {|Content-Type: application/octet-stream|}; {||}; {|aωb|}
+    ; {|Content-Type: application/octet-stream|}
+    ; {||}
+    ; {|aωb|}
     ; {|-----------------------------735323031399963166993862150--|} ]
-    |> String.concat "\r\n" in
-  match
-    Http_multipart_formdata.parse ~content_type_header ~body:(`String body)
-  with
-  | parts ->
+    |> String.concat "\r\n"
+  in
+  match Http_multipart_formdata.parse ~content_type_header ~body with
+  | parts       ->
       List.length (SM.find "text1" parts) = 2
       && List.length (SM.find "file1" parts) = 3
       && SM.cardinal parts = 2
