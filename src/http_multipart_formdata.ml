e1b8bca590e9fab074641f6c22a34e1c01c2fc88
Bikal Lem <gbikal@gmail.com>
2021-06-15 22:46:46 +0100

    update copyright notice

--- src/http_multipart_formdata.ml
+++ src/http_multipart_formdata.ml
@@ -1,5 +1,5 @@
 (*-------------------------------------------------------------------------
- * Copyright (c) 2019, 2020 Bikal Gurung. All rights reserved.
+ * Copyright (c) 2020, 2021 Bikal Gurung. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License,  v. 2.0. If a copy of the MPL was not distributed with this
