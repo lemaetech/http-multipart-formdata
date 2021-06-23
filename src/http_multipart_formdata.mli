b79df090fdeb0c12ae89b0d4200c5db07df8a0b2
Bikal Lem <gbikal@gmail.com>
2021-06-23 13:21:16 +0100

    document function

--- src/http_multipart_formdata.mli
+++ src/http_multipart_formdata.mli
@@ -20,55 +20,12 @@ module Part_header : sig
   val pp : Format.formatter -> t -> unit
 end
 
+(** Represents the multipart boundary value. *)
 type boundary = string
 
 val parse_boundary : content_type:string -> (boundary, string) result Lwt.t
-
-(** [parse ~content_type_header ~body part_handler] parses [body] and streams
-    [part_header] and [part_body_data] to [part_handler].
-
-    [content_type_header] is the HTTP request [Content-Type] header value. It is
-    used to parse a [boundary] value.
-
-    [body] is the raw HTTP POST request body content stream.
-
-    {4 Examples}
-
-    {[
-      module M = Http_multipart_formdata
-
-      ;;
-      let content_type_header =
-        "multipart/form-data; \
-         boundary=---------------------------735323031399963166993862150" in
-      let body =
-        [ {||}; {|-----------------------------735323031399963166993862150|}
-        ; {|Content-Disposition: form-data; name="text1"|}; {||}
-        ; {|text default|}
-        ; {|-----------------------------735323031399963166993862150|}
-        ; {|Content-Disposition: form-data; name="text2"|}; {||}; {|aωb|}
-        ; {|-----------------------------735323031399963166993862150|}
-        ; {|Content-Disposition: form-data; name="file1"; filename="a.txt"|}
-        ; {|Content-Type: text/plain|}; {||}; {|Content of a.txt.|}; {||}
-        ; {|-----------------------------735323031399963166993862150|}
-        ; {|Content-Disposition: form-data; name="file2"; filename="a.html"|}
-        ; {|Content-Type: text/html|}; {||}
-        ; {|<!DOCTYPE html><title>Content of a.html.</title>|}; {||}
-        ; {|-----------------------------735323031399963166993862150|}
-        ; {|Content-Disposition: form-data; name="file3"; filename="binary"|}
-        ; {|Content-Type: application/octet-stream|}; {||}; {|aωb|}
-        ; {|-----------------------------735323031399963166993862150--|} ]
-        |> String.concat "\r\n" in
-      let mp = M.parse ~content_type_header ~body in
-      let file1_1 = M.Map.find "file1" mp in
-      let file1_2 =
-        [ { M.Part.body= Bytes.of_string "\r\nContent of a.txt.\r\n\r\n"
-          ; name= "file1"
-          ; content_type= "text/plain"
-          ; filename= Some "a.txt"
-          ; parameters= M.Map.empty } ] in
-      M.equal_parts file1_1 file1_2
-    ]} *)
+(** [parse_boundary ~content_type] parses [content_type] to extract [boundary]
+    value.[content_type] is the HTTP request [Content-Type] header value. *)
 
 val parse_parts :
      ?part_stream_chunk_size:int
@@ -76,3 +33,9 @@ val parse_parts :
   -> on_part:(Part_header.t -> char Lwt_stream.t -> unit Lwt.t)
   -> char Lwt_stream.t
   -> (unit, string) result Lwt.t
+(** [parse ~part_stream_chunk_size ~boundary ~on_part body] parses [body] and
+    streams [part_header] and [part_body_data] to [on_part].
+
+    [boundary] is part boundary value
+
+    [body] is the raw HTTP POST request body content stream. *)
